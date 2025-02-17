0886e2fa5f1a3b1983be73edf45902f24e7a2db0
gounthar <gounthar@gmail.com>
2025-02-17 17:41:56 +0100

    refactor(GitHubParameters): Improve code formatting and indentation

--- src/main/java/io/jenkins/plugins/github/GitHubParameters.java
+++ src/main/java/io/jenkins/plugins/github/GitHubParameters.java
@@ -1,10 +1,11 @@
 package io.jenkins.plugins.github;
 
 public interface GitHubParameters {
-  String getCredentialId();
-  void setCredentialId(String credentialId);
+    String getCredentialId();
 
+    void setCredentialId(String credentialId);
 
-  String getGithubServer();
-  void setGithubServer(String gitHubServer);
+    String getGithubServer();
+
+    void setGithubServer(String gitHubServer);
 }
